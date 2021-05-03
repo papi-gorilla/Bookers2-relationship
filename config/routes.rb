92c410f2a4911fbda20ef71e63a81e3f02f1bbe6
papi-gorilla <jindeensadayoshi@gmail.com>
2021-05-03 14:20:18 +0000

    [Finish] フォローワー一覧ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about" => "homes#about"
   resources :books, only:[:index, :create, :show, :edit, :update, :destroy]
+  resources :users do
+    member do
+      get :following, :follower
+    end
+  end
   resources :users, only:[:index, :show, :edit, :update]
   post "follow/:id" => "relationships#create", as: "follow"
   delete "unfollow/:id" => "relationships#destroy", as: "unfollow"
+
 end
